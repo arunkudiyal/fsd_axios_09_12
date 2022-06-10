21395208b5618281f5f88b989ad46c663c829993
arunkudiyal <arunkudiyal@icloud.com>
2022-06-10 10:15:14 +0530

    GET requests from Axios

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -2,13 +2,16 @@ import React from 'react';
 
 import './Post.css';
 
-const post = (props) => (
-    <article className="Post">
-        <h1>Title</h1>
-        <div className="Info">
-            <div className="Author">Author</div>
-        </div>
-    </article>
+const post = (props) => ( <
+    article onClick = { props.clicked }
+    className = "Post" >
+    <
+    h1 > { props.title } < /h1> <
+    div className = "Info" >
+    <
+    div className = "Author" > { props.author } < /div> <
+    /div> <
+    /article>
 );
 
 export default post;
--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -9,44 +9,61 @@ import './Blog.css';
 
 class Blog extends Component {
     state = {
-        posts: []
+        posts: [],
+        selectedPostId: null
     }
 
     // JavaScript --> API Call to an URL
     componentDidMount() {
         // Call any API / Cause side-effect
         axios.get('https://jsonplaceholder.typicode.com/posts')
-            .then(response =>
-                this.setState({ posts: response.data })
-                // console.log(response.data)
+            .then(response => {
+                    const posts = response.data.slice(0, 8)
+                        // Update the API data by adding the value of Author
+                    const updatedPosts = posts.map(post => {
+                        return {
+                            ...post,
+                            author: 'Arun Kudiyal'
+                        }
+                    })
+                    this.setState({ posts: updatedPosts })
+                }
+                // console.log(response)
             )
             .catch(err => console.log(err))
     }
 
+    selectedPostHandler = (id) => {
+        this.setState({ selectedPostId: id })
+    }
+
     render() {
+        const posts = this.state.posts.map(post => {
+            return <Post
+            key = { post.id }
+            title = { post.title }
+            author = { post.author }
+            clicked = {
+                () => this.selectedPostHandler(post.id) }
+            />
+        })
+
         return ( <
             div >
             <
-            section className = "Posts" >
-            <
-            Post / >
-            <
-            Post / >
-            <
-            Post / >
-            <
-            /section> <
+            section className = "Posts" > { posts } <
+            /section>  <
             section >
             <
-            FullPost / >
-            <
-            /section> <
+            FullPost id = { this.state.selectedPostId }
+            /> <
+            /section>  <
             section >
             <
             NewPost / >
             <
-            /section> <
-            /div>
+            /section>  <
+            /div >
         );
     }
 }
